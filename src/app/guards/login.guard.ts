f7cc7319819dc21e5f963b2f838141a894c873d6
paulolemos-overflow <paulo.lemos@bringglobal.com>
2023-01-07 00:21:38 -0100

    guard

--- src/app/guards/login.guard.ts
+++ src/app/guards/login.guard.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+
+import { LoginService } from '../services/login.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginGuard implements CanActivate {
 
+  guardBoolean!: boolean;
+
+  constructor(private loginService: LoginService, private router: Router) {console.log("??????")}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      console.log("guardou e agora é p jantar");
-    return true;
+
+    if(localStorage.getItem('data_login')) {
+      return true;
+    } else { return false }
+
   }
 
 }
